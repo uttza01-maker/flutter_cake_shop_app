47696e8002588e4a05f8e2a3405d1eae82780a4a
piyapan kummoon <uttza01@gmail.com>
2026-04-19 12:18:21 +0700

    flutter_cake_shop_app

---
+++ lib/models/cake_shop.dart
@@ -0,0 +1,31 @@
+class CakeShop{
+  //ตัวแปรที่แมปกับรายละเอียดของข้อมูล
+  String? name;
+  String? address;
+  String? phone;
+  String? image1;
+  String? image2;
+  String? image3;
+  String? description;
+  String? openCloseTime;
+  String? website;
+  String? facebook;
+  String? latitude;
+  String? longitude;
+ 
+  //Constructor
+  CakeShop({
+    this.name,
+    this.address,
+    this.phone,
+    this.image1,
+    this.image2,
+    this.image3,
+    this.description,
+    this.openCloseTime,
+    this.website,
+    this.facebook,
+    this.latitude,
+    this.longitude,
+  });
+}
